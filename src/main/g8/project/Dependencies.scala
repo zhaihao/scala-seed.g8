14343fa27f33682149c6c3843202e1ef510e1158
zhaihao <zhaihao@ooon.me>
2022-09-06 15:59:14 +0800

    Update Dependencies.scala

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -42,11 +42,11 @@ object Dependencies extends AutoPlugin {
       "org.scalatest" %% "scalatest-freespec"       % "3.2.13"
     )
    
-     lazy val overrideDeps = Seq(
+     lazy val overrides = Seq(
       // "org.scala-lang.modules" %% "scala-xml" % "2.0.1"
     )
 
-    lazy val excludeDeps = Seq(
+    lazy val excludes = Seq(
       "org.slf4j" % "slf4j-log4j12",
       "log4j"     % "log4j"
   }
